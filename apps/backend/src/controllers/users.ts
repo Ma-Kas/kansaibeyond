45c60bbc89c9d48ab541ef0d69a2e9be77948eb7
Ma-Kas <115688479+Ma-kas@users.noreply.github.com>
2024-02-28 16:19:41 +0100

    (be): Implement users get, put, delete routes
    Add basic routes for get, put and delete requests on users api endpoint;
    Add simple REST Client tests for route testing;
    
    Notes:
    - Routes are missing any form of authorization

--- apps/backend/src/controllers/users.ts
+++ apps/backend/src/controllers/users.ts
@@ -2,14 +2,57 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 
 import { User } from '../models';
-import { validateNewUser } from '../utils/inputValidation';
+import { validateNewUser, validateUserUpdate } from '../utils/inputValidation';
 
 import BadRequestError from '../errors/BadRequestError';
+import NotFoundError from '../errors/NotFoundError';
 
 const router = express.Router();
 
 // Disabling this rule since solving error would require .then.catch
 // or update to Express 5 (currently beta), or typecast as RequestHandler
+// eslint-disable-next-line  @typescript-eslint/no-misused-promises
+router.get('/', async (_req, res, next) => {
+  try {
+    const allUsers = await User.findAll({
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+      // include: [
+      //   {
+      //     model: Blog,
+      //     include: [
+      //       {
+      //         model: Category,
+      //         attributes: ['categoryName'],
+      //       },
+      //     ],
+      //     attributes: {
+      //       exclude: ['userId'],
+      //     },
+      //   },
+      // ],
+    });
+
+    res.status(200).json(allUsers);
+  } catch (err: unknown) {
+    next(err);
+  }
+});
+
+// eslint-disable-next-line  @typescript-eslint/no-misused-promises
+router.get('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username },
+    });
+    if (!user) {
+      throw new NotFoundError({ message: 'User not found.' });
+    }
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    next(err);
+  }
+});
+
 // eslint-disable-next-line  @typescript-eslint/no-misused-promises
 router.post('/', async (req, res, next) => {
   try {
@@ -21,6 +64,55 @@ router.post('/', async (req, res, next) => {
     } else {
       throw new BadRequestError({ message: 'Invalid User data.' });
     }
+  } catch (err: unknown) {
+    next(err);
+  }
+});
+
+// eslint-disable-next-line  @typescript-eslint/no-misused-promises
+router.put('/:username', async (req, res, next) => {
+  try {
+    const userToUpdate = await User.findOne({
+      where: { username: req.params.username },
+    });
+    if (!userToUpdate) {
+      throw new NotFoundError({ message: 'User to update was not found.' });
+    }
+    const userUpdateData = validateUserUpdate(req.body);
+    if (userUpdateData) {
+      // Only re-crypt password if password has changed
+      if ('password' in userUpdateData) {
+        userUpdateData.password = await bcrypt.hash(
+          userUpdateData.password!,
+          10
+        );
+      }
+      const updatedUser = await User.update(userUpdateData, {
+        where: { username: userToUpdate.username },
+        returning: true,
+      });
+      res.status(200).json(updatedUser[1][0]);
+    } else {
+      // No update data => return original user
+      res.status(204).send();
+    }
+  } catch (err: unknown) {
+    next(err);
+  }
+});
+
+// eslint-disable-next-line  @typescript-eslint/no-misused-promises
+router.delete('/:username', async (req, res, next) => {
+  try {
+    const userToDelete = await User.findOne({
+      where: { username: req.params.username },
+    });
+    if (!userToDelete) {
+      throw new NotFoundError({ message: 'User to delete was not found.' });
+    }
+
+    await userToDelete.destroy();
+    res.status(200).json({ message: `Deleted ${userToDelete.username}` });
   } catch (err: unknown) {
     next(err);
   }
--- apps/backend/src/types/types.ts
+++ apps/backend/src/types/types.ts
@@ -2,13 +2,25 @@ type User = {
   id: number;
   username: string;
   userIcon: string | null;
+  firstName: string;
+  lastName: string;
+  email: string;
+  displayName: string;
   password: string;
   status: 'Admin' | 'Writer' | 'Tech' | 'Guest';
   disabled: boolean;
-  blogId: number[];
-  commentId: number[];
+  blogId?: number[];
+  commentId?: number[];
 };
 
-type NewUser = Pick<User, 'username' | 'password'>;
+type NewUser = Pick<
+  User,
+  'username' | 'firstName' | 'lastName' | 'email' | 'displayName' | 'password'
+>;
 
-export { NewUser };
+// Omit original userIcon, re-add in intersection to disallow null when updating
+type UpdateUser = Partial<
+  Omit<User, 'id' | 'userIcon' | 'status' | 'disabled' | 'blogId' | 'commentId'>
+> & { userIcon?: string };
+
+export { User, NewUser, UpdateUser };
