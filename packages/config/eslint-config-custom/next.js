8e41beb3954dcf673a764b55e14d9019b61210a1
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-06-13 15:56:44 +0200

    Update config for Next.js

---
+++ packages/config/eslint-config-custom/next.js
@@ -0,0 +1,45 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    'prettier',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    JSX: true,
+  },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  rules: {
+    '@typescript-eslint/semi': ['error'],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-case-declarations': 'off',
+  },
+
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    '.*.js',
+    'node_modules/',
+  ],
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+};
