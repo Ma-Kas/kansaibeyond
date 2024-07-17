a3ccb3d0894914041972a3bdadef627ea0ef34fb
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-07-17 15:41:58 +0200

    (fe): Add Searchbar component

---
+++ apps/next-frontend/src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+/* eslint @typescript-eslint/no-unsafe-assignment: 0 */
+// Necessary due to Next.js typing svg in Image component as "any"
+
+import Image from 'next/image';
+import cx from 'clsx';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import searchIcon from '@public/images/search.svg';
+
+import classes from './Searchbar.module.css';
+
+const Searchbar = ({ inHeader }: { inHeader: boolean }) => {
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const handleSearch = (searchTerm: string) => {
+    if (searchTerm) {
+      params.set('q', searchTerm);
+    } else {
+      params.delete('q');
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (pathname === '/search') {
+      router.replace(`${pathname}?${params.toString()}`);
+    } else {
+      router.push(`/search?${params.toString()}`);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={
+        // eslint-disable-next-line  @typescript-eslint/no-unsafe-call
+        cx(classes.form, {
+          [classes['form_header']]: inHeader,
+        })
+      }
+    >
+      <input
+        className={classes.input}
+        name='search'
+        placeholder='Search for posts ...'
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('q')?.toString()}
+        aria-label='Search the page'
+      />
+      <button
+        className={classes.button}
+        type='submit'
+        aria-label='magnifying glass icon'
+      >
+        <Image className={classes.icon} src={searchIcon} alt='' />
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
