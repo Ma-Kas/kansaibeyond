ed7eee1f8755ad9bd680a60b222076ba6b8c2013
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-07-19 13:59:25 +0200

    (fe): Add CategoryBannerSkeleton to loading

--- apps/next-frontend/src/components/Skeletons/AffiliateCardSkeleton.tsx
+++ apps/next-frontend/src/components/Skeletons/AffiliateCardSkeleton.tsx
@@ -3,10 +3,8 @@ import classes from './Skeletons.module.css';
 const AffiliateCardSkeleton = () => {
   return (
     <article className={`${classes['affiliate_card']} ${classes['shimmer']}`}>
-      <div className={classes['affiliate_content']}>
-        <div></div>
-        <div></div>
-      </div>
+      <div></div>
+      <div></div>
     </article>
   );
 };
