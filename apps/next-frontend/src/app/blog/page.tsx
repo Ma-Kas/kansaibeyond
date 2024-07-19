9f85cfc4f3d573a3d2398b992e0578858872207d
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-07-19 15:19:02 +0200

    (fe): Implement loading in CategoryGridSection

--- apps/next-frontend/src/app/blog/page.tsx
+++ apps/next-frontend/src/app/blog/page.tsx
@@ -1,14 +1,21 @@
+import { Suspense } from 'react';
 import { getAllPosts } from '@/lib/requests/postRequests';
 
 import SectionHeading from '@/components/SectionHeading/SectionHeading';
 import PostGridSection from '@/components/PostGridSection/PostGridSection';
 import CategoryGridSection from '@/components/CategoryGridSection/CategoryGridSection';
+import CategoryGridSectionSkeleton from '@/components/CategoryGridSection/CategoryGridSkeleton';
 import FeaturedPost from '@/components/FeaturedPost/FeaturedPost';
 
 import classes from './blog.module.css';
 
 const BlogHubPage = async () => {
   const posts = await getAllPosts('?limit=7');
+  const categorySectionHeading = (
+    <SectionHeading>
+      <span>explore</span>&nbsp;categories
+    </SectionHeading>
+  );
 
   return (
     <>
@@ -18,16 +25,22 @@ const BlogHubPage = async () => {
         </SectionHeading>
         <FeaturedPost post={posts[0]} />
       </section>
+      {/* Post Card Grid */}
       <PostGridSection posts={posts.slice(1)} withViewMoreLink={true}>
         <SectionHeading>
           <span>recent</span>&nbsp;posts
         </SectionHeading>
       </PostGridSection>
-      <CategoryGridSection>
-        <SectionHeading>
-          <span>explore</span>&nbsp;categories
-        </SectionHeading>
-      </CategoryGridSection>
+      {/* Category Card Grid */}
+      <Suspense
+        fallback={
+          <CategoryGridSectionSkeleton>
+            {categorySectionHeading}
+          </CategoryGridSectionSkeleton>
+        }
+      >
+        <CategoryGridSection>{categorySectionHeading}</CategoryGridSection>
+      </Suspense>
     </>
   );
 };
