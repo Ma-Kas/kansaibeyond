a6117b4a835367b331abfbde4a0499dff2437a89
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-07-22 17:41:49 +0200

    (fe): Implement root 404 not found page

---
+++ apps/next-frontend/src/components/ErrorPages/NotFoundPage.tsx
@@ -0,0 +1,40 @@
+/* eslint @typescript-eslint/no-unsafe-assignment: 0 */
+// Necessary due to Next.js typing svg in Image component as "any"
+
+import Image from 'next/image';
+import errorPanda from '@public/images/red_panda_error.svg';
+import classes from './ErrorPages.module.css';
+import Link from 'next/link';
+
+const NotFoundPage = () => {
+  return (
+    <section className={classes['error_section']}>
+      <article className={classes['error_content']}>
+        <p className={classes.label}>404</p>
+        <h1 className={classes.title}>Page not found</h1>
+
+        <Image
+          className={classes['error_image']}
+          src={errorPanda}
+          alt=''
+          priority={true}
+        />
+
+        <p className={classes.description}>
+          Seems like the page you are looking for does not exist. It might have
+          been removed, had its name changed, or is temporarily unavailable.
+        </p>
+        <Link
+          className={classes['button_link']}
+          title='Home'
+          href={'/'}
+          aria-label='Return to home'
+        >
+          Go to homepage
+        </Link>
+      </article>
+    </section>
+  );
+};
+
+export default NotFoundPage;
