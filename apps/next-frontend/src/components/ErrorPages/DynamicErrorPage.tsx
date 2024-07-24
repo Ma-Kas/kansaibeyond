2fcf63dcabc40493277bb65bc651ebdabac6bbd2
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-07-24 13:57:11 +0200

    (fe): Add partial error element in about page

--- apps/next-frontend/src/components/ErrorPages/DynamicErrorPage.tsx
+++ apps/next-frontend/src/components/ErrorPages/DynamicErrorPage.tsx
@@ -4,46 +4,9 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import errorPanda from '@public/images/red_panda_error.svg';
-import { ERRORS_DICTIONARY } from '@/utils/backend-error-response-validation';
+import { createFriendlyErrorData } from '@/utils/create-friendly-error-data';
 
 import classes from './ErrorPages.module.css';
-
-const createFriendlyErrorData = (message: string) => {
-  switch (message) {
-    case ERRORS_DICTIONARY.BAD_REQUEST: {
-      return {
-        status: 400,
-        name: message,
-        description:
-          "Seems like there is something wrong with the data. That's not on you, it's on us. It might be temporary, so you can either try to reload the page, or go back.",
-      };
-    }
-    case ERRORS_DICTIONARY.NOT_FOUND: {
-      return {
-        status: 404,
-        name: message,
-        description:
-          "Seems like we couldn't find some data you requested. It might have been removed, had its name changed, or is temporarily unavailable.",
-      };
-    }
-    case ERRORS_DICTIONARY.SERVER_ERROR: {
-      return {
-        status: 500,
-        name: message,
-        description:
-          'Seems like there was an error connecting to the server. Maybe it is down, or there is an issue with the connection. It might be temporary, so you can either try to reload the page, or go back.',
-      };
-    }
-    default: {
-      return {
-        status: 500,
-        name: message,
-        description:
-          "Seems like something just went very wrong. It can happen, and it just did. We're not sure why it happened, but you can try to reload the page, or go back.",
-      };
-    }
-  }
-};
 
 const DynamicErrorPage = ({
   errorMessage,
