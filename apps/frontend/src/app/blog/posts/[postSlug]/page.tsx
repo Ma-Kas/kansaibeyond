d0db5e593b2229ffbb847dae431add9b0ed5a286
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-08-09 17:41:41 +0200

    (fe): Add generateMetadata to dynamic pages

--- apps/frontend/src/app/blog/posts/[postSlug]/page.tsx
+++ apps/frontend/src/app/blog/posts/[postSlug]/page.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import type { Metadata } from 'next';
 import { getOnePost } from '@/lib/requests/postRequests';
 import BlogPostView from '@/components/BlogPostView/BlogPostView';
 
@@ -9,6 +10,19 @@ const DynamicScrollToTop = dynamic(() => import('@/components/ScrollToTop'), {
 // export const generateStaticParams = async () => {
 //   const postSlugs = await getPostSlugList();
 //   return postSlugs;
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { postSlug: string };
+}): Promise<Metadata> => {
+  const post = await getOnePost(params.postSlug);
+
+  return {
+    title: post.title,
+    description: 'Read through this exciting post.',
+  };
+};
 
 const PostPage = async ({
   params: { postSlug },
