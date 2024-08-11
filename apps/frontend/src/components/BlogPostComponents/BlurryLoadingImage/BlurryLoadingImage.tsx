c34a2c9a660f762133c95e2176a356ffd9a9fe26
Ma-Kas <115688479+Ma-Kas@users.noreply.github.com>
2024-08-11 13:45:41 +0200

    (fe): Add blurry loading image in PostImage.tsx
    Unable to use next/image due to unknown image parameters from cms,
    thus manual implementation of blurry placeholder loading;
    Unable to use srcset and sizes due to unknown image parameters from cms;
    
    Add wsrv.nl as caching provider to reduce egress @Cloudinary;

---
+++ apps/frontend/src/components/BlogPostComponents/BlurryLoadingImage/BlurryLoadingImage.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ImageNode } from '@/types/post-content-types';
+import {
+  CLOUDINARY_BASE_URL,
+  WSRV_BASE_URL,
+  WSRV_TRANSFORM,
+  LOADING_PLACEHOLDER_IMAGE_TRANSFORM,
+  POST_SINGLE_IMAGE_TRANSFORM,
+} from '@/config/constants';
+
+type Props = {
+  className: string;
+  node: ImageNode;
+};
+
+const BlurryLoadingImage = ({ className, node }: Props) => {
+  // Optimise image loading further, pass this into fetchImageTransform
+  // Will optimise size, but quality suffers visibly. Leaving here in case
+  // const imageWidth = node.maxWidth ? Number(node.maxWidth.slice(0, -2)) : 740;
+
+  const [currentImage, setCurrentImage] = useState(
+    `${WSRV_BASE_URL}${CLOUDINARY_BASE_URL}${LOADING_PLACEHOLDER_IMAGE_TRANSFORM}${node.src}${WSRV_TRANSFORM}`
+  );
+  const [loading, setLoading] = useState(true);
+
+  const fetchImage = (src: string) => {
+    const loadingImage = new Image();
+    loadingImage.src = src;
+    loadingImage.onload = () => {
+      setCurrentImage(loadingImage.src);
+      setLoading(false);
+    };
+  };
+
+  useEffect(() => {
+    fetchImage(
+      `${WSRV_BASE_URL}${CLOUDINARY_BASE_URL}${POST_SINGLE_IMAGE_TRANSFORM}${node.src}${WSRV_TRANSFORM}`
+    );
+  }, [node.src]);
+
+  return (
+    <img
+      style={{
+        filter: `${loading ? 'blur(20px)' : ''}`,
+        transition: '0.5s filter linear',
+      }}
+      className={className}
+      src={currentImage}
+      alt={node.altText}
+    />
+  );
+};
+
+export default BlurryLoadingImage;
